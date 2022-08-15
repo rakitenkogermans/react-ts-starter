b047fc467713c6c5fc25c06af3adc90499a268a0
rakitenkogermans <rakitenko.germans@gmail.com>
2022-08-15 11:25:10 +0300

    ts, babel, root component

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, {FC} from 'react';
+
+type AppProps = {};
+
+const App: FC<AppProps> = ({}) => {
+    return (
+        <div>
+            App
+        </div>
+    );
+};
+
+export {App};
