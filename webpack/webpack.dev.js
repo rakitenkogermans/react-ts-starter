70d35c9804fa8c4dbc445b85b06387b6da42abfd
rakitenkogermans <rakitenko.germans@gmail.com>
2022-08-15 15:36:59 +0300

    react refresh

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,11 +1,18 @@
 const Dotenv = require("dotenv-webpack");
 const path = require("path");
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = {
+    mode: "development",
+    devServer: {
+        hot: true,
+        open: true,
+    },
+    devtool: "cheap-module-source-map",
     plugins: [
         new Dotenv({
             path: path.resolve(__dirname,'../.env.development')
-        })],
-    mode: "development",
-    devtool: "cheap-module-source-map",
+        }),
+        new ReactRefreshWebpackPlugin(),
+    ],
 }
